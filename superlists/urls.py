9a24f59259897ddaea0fbc810fd84cb75368d1f5
Michael Allan <michaelallan@optushome.com.au>
2013-04-04 19:59:09 +1100

    Reformat for lint validation

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
+
     # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
     # url(r'^superlists/', include('superlists.foo.urls')),
